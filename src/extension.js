e6ff026f157df6075b8fc2a65a8b1f5b92c30e7d
nonoroazoro <xiaochao.k@gmail.com>
2016-10-14 10:36:38 +0800

    add test

--- src/extension.js
+++ src/extension.js
@@ -1,31 +1,40 @@
 const vscode = require("vscode");
+const Config = require("./Config");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(p_context)
 {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log("Congratulations, your extension 'syncing' is now active!");
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand("syncing.uploadSettings", () =>
-    {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage("Hello Jackasdads !");
-    });
-
-    p_context.subscriptions.push(disposable);
+    const config = _initConfig(p_context);
+    _initCommands(p_context);
 }
 
-// this method is called when your extension is deactivated
-function deactivate()
+function _initConfig(p_context)
 {
+    return new Config(p_context);
+}
+
+function _initCommands(p_context)
+{
+    // 1. downloadSettings
+    _registerCommand(p_context, "syncing.downloadSettings", () =>
+    {
+        vscode.window.showInformationMessage("Download Settings!");
+    });
+
+    // 2. uploadSettings
+    _registerCommand(p_context, "syncing.uploadSettings", () =>
+    {
+        vscode.window.showInformationMessage("Upload Settings!");
+    });
+
+    vscode.commands.executeCommand("syncing.downloadSettings");
+}
+
+/**
+ * wrap registerCommand.
+ */
+function _registerCommand(p_context, p_command, p_callback)
+{
+    p_context.subscriptions.push(vscode.commands.registerCommand(p_command, p_callback));
 }
 
 module.exports.activate = activate;
-module.exports.deactivate = deactivate;
