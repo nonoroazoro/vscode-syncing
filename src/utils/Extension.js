0dcca7455057fc91ba07193ed8625bf84a5837a8
nonoroazoro <xiaochao.k@gmail.com>
2016-10-18 16:41:54 +0800

    add install

--- src/utils/Extension.js
+++ src/utils/Extension.js
@@ -27,6 +27,36 @@ function getAll(p_includeBuiltin = false)
     return result;
 }
 
+
+/**
+ * install extensions.
+ * @param {string|Array} p_list extension id or extension id list.
+ */
+function install(p_extensions)
+{
+    return new Promise((p_resolve, p_reject) =>
+    {
+        if (p_extensions)
+        {
+            let extensions = p_extensions;
+            if (typeof p_extensions === "string")
+            {
+                extensions = [p_extensions];
+            }
+
+            if (Array.isArray(extensions))
+            {
+                // TODO: install extension.
+                // for (let ext of extensions)
+                // {
+                // }
+            }
+        }
+        p_resolve();
+    });
+}
+
 module.exports = {
-    getAll
+    getAll,
+    install
 };
