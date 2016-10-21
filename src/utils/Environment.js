2f1b63bac0594fc95c52a755901448e102859e3b
nonoroazoro <xiaochao.k@gmail.com>
2016-10-21 12:49:46 +0800

    add Environment

---
+++ src/utils/Environment.js
@@ -0,0 +1,99 @@
+/**
+ * vscode's environment.
+ */
+
+const os = require("os");
+const path = require("path");
+
+class Environment
+{
+    constructor(p_context)
+    {
+        this._isMac = process.platform === "darwin";
+        this._isInsiders = p_context.extensionPath.includes("insiders");
+        this._extensionsPath = path.join(
+            os.homedir(),
+            this._isInsiders ? ".vscode-insiders" : ".vscode",
+            "extensions"
+        );
+        this._codeBasePath = this._getCodeBasePath(this._isInsiders);
+        this._codeUserPath = path.join(this._codeBasePath, "User");
+    }
+
+    /**
+     * check if mac.
+    */
+    get isMac()
+    {
+        return this._isMac;
+    }
+
+    /**
+     * check if vscode is an insiders version.
+     */
+    get isInsiders()
+    {
+        return this._isInsiders;
+    }
+
+    /**
+     * get vscode's extensions base path.
+     */
+    get extensionsPath()
+    {
+        return this._extensionsPath;
+    }
+
+    /**
+     * get vscode's config base path.
+     */
+    get codeBasePath()
+    {
+        return this._codeBasePath;
+    }
+
+    _getCodeBasePath(p_isInsiders)
+    {
+        let basePath;
+        switch (process.platform)
+        {
+            case "win32":
+                basePath = process.env.APPDATA;
+                break;
+
+            case "darwin":
+                basePath = path.join(process.env.HOME, "Library/Application Support");
+                break;
+
+            case "linux":
+                basePath = path.join(os.homedir(), ".config");
+                break;
+
+            default:
+                basePath = "/var/local";
+        }
+        return path.join(basePath, p_isInsiders ? "Code - Insiders" : "Code");
+    }
+
+    /**
+     * get vscode's config `User` path.
+     */
+    get codeUserPath()
+    {
+        return this._codeUserPath;
+    }
+}
+
+let _instance;
+function create(p_context)
+{
+    if (_instance === undefined)
+    {
+        _instance = new Environment(p_context);
+    }
+    return _instance;
+}
+
+module.exports = {
+    create
+};
