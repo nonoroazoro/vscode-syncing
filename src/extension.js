2d6aaf043ce6fcf1b17b4c6d816520b4c7728c03
nonoroazoro <xiaochao.k@gmail.com>
2016-10-21 19:32:06 +0800

    update

--- src/extension.js
+++ src/extension.js
@@ -1,15 +1,10 @@
-const async = require("async");
 const vscode = require("vscode");
 
 const Gist = require("./utils/Gist");
-const Config = require("./utils/Config");
 const Toast = require("./utils/Toast");
+const Config = require("./utils/Config");
 
-let _api;
 let _config;
-let _token;
-let _gistID;
-let _settings;
 
 function activate(p_context)
 {
@@ -17,65 +12,13 @@ function activate(p_context)
     _initCommands(p_context);
 }
 
+/**
+ * init global variables.
+ * @param {Object} p_context
+ */
 function _initGlobals(p_context)
 {
-    // TODO:
-    _config = new Config(p_context);
-    _prepareSyncingSettings(_config).then(() =>
-    {
-        console.log("");
-    }).catch(() =>
-    {
-        Toast.statusInfo("Syncing: canceled as Github Access Token or Gist ID isn't set.");
-    });
-
-    // Syncing's config.
-    _gistID = "";
-    _token = "";
-    _api = new Gist(_token, { proxy: "http://127.0.0.1:1080" });
-}
-
-function _prepareSyncingSettings(p_config)
-{
-    return new Promise((p_resolve, p_reject) =>
-    {
-        _settings = p_config.loadSyncingSettings();
-
-        const tasks = [];
-        if (!_settings.token)
-        {
-            tasks.push(Toast.showGitHubTokenInputBox);
-        }
-        if (!_settings.id)
-        {
-            tasks.push(Toast.showGistInputBox);
-        }
-        async.eachSeries(
-            tasks,
-            (task, done) =>
-            {
-                task().then((value) =>
-                {
-                    Object.assign(_settings, value);
-                    done();
-                }).catch((err) =>
-                {
-                    done(err);
-                });
-            },
-            (err) =>
-            {
-                if (err || !_settings.token || !_settings.id)
-                {
-                    p_reject();
-                }
-                else
-                {
-                    p_resolve();
-                }
-            }
-        );
-    });
+    _config = Config.create(p_context);
 }
 
 function _initCommands(p_context)
@@ -98,20 +41,36 @@ function _registerCommand(p_context, p_command, p_callback)
 function _uploadSettings()
 {
     Toast.status("Syncing: gathering local settings...");
-    _config.getConfigs({ load: true }).then((uploads) =>
+    _config.prepareSyncingSettings(false).then((settings) =>
     {
-        Toast.status("Syncing: uploading settings...");
-        _api.findAndUpdate(_gistID, uploads).then((gist) =>
+        const api = Gist.create(settings.token, "http://127.0.0.1:1080");
+        _config.getConfigs({ load: true }).then((uploads) =>
         {
-            // TODO: check if auto created a new gist id, save it!
-            Toast.statusInfo("Syncing: settings uploaded.");
-        }).catch(() =>
+            Toast.status("Syncing: uploading settings...");
+            api.findAndUpdate(settings.id, uploads).then((gist) =>
+            {
+                if (gist.id === settings.id)
+                {
+                    Toast.statusInfo("Syncing: settings uploaded.");
+                }
+                else
+                {
+                    _config.saveSyncingSettings(Object.assign({}, settings, { id: gist.id })).then(() =>
+                    {
+                        Toast.statusInfo("Syncing: settings uploaded.");
+                    });
+                }
+            }).catch(() =>
+            {
+                Toast.statusError("Syncing: upload failed, please check your Internet connection.");
+            });
+        }).catch((err) =>
         {
-            Toast.statusError("Syncing: upload failed, please check your Internet connection.");
+            Toast.statusError(`Syncing: upload failed: ${err.message}`);
         });
-    }).catch((err) =>
+    }).catch(() =>
     {
-        Toast.statusError(`Syncing: upload failed: ${err.message}`);
+        Toast.statusInfo("Syncing: canceled as Github Access Token or Gist ID isn't set.");
     });
 }
 
@@ -121,19 +80,27 @@ function _uploadSettings()
 function _downloadSettings()
 {
     Toast.status("syncing: checking remote settings...");
-    _api.get(_gistID).then((gist) =>
+    _config.prepareSyncingSettings().then((settings) =>
     {
-        Toast.status("syncing: downloading settings...");
-        _config.saveConfigs(gist.files).then((saved) =>
+        const api = Gist.create(settings.token, "http://127.0.0.1:1080");
+        api.get(settings.id).then((gist) =>
         {
-            Toast.statusInfo("Syncing: settings downloaded.");
+            Toast.status("syncing: downloading settings...");
+            _config.saveConfigs(gist.files).then((saved) =>
+            {
+                // TODO: log saved files.
+                Toast.statusInfo("Syncing: settings downloaded.");
+            }).catch((err) =>
+            {
+                Toast.statusError(`Syncing: download failed: ${err.message}`);
+            });
         }).catch((err) =>
         {
-            Toast.statusError(`Syncing: download failed: ${err.message}`);
+            Toast.statusError(`Syncing: download failed. ${err.message}`);
         });
     }).catch(() =>
     {
-        Toast.statusError("Syncing: download failed, please check your Internet connection.");
+        Toast.statusInfo("Syncing: canceled as Github Access Token or Gist ID isn't set.");
     });
 }
 
