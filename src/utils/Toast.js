08d33ce838dd92fea997d4fa1798696cc3ada61d
nonoroazoro <xiaochao.k@gmail.com>
2016-10-21 17:24:37 +0800

    add box

--- src/utils/Toast.js
+++ src/utils/Toast.js
@@ -40,8 +40,66 @@ function statusError(p_message)
     status(p_message, 8000);
 }
 
+/**
+ * show GitHub access token box.
+ * @returns {Promise}
+ */
+function showGitHubTokenInputBox()
+{
+    return new Promise((p_resolve, p_reject) =>
+    {
+        const options = {
+            placeHolder: "Enter Your Github Personal Access Token.",
+            password: false,
+            prompt: "Used to access your GitHub account.",
+            ignoreFocusOut: true
+        };
+        vscode.window.showInputBox(options).then((value) =>
+        {
+            if (value)
+            {
+                p_resolve({ token: value.trim() });
+            }
+            else
+            {
+                p_reject(new Error("Canceled."));
+            }
+        });
+    });
+}
+
+/**
+ * show Gist id box.
+ * @returns {Promise}
+ */
+function showGistInputBox()
+{
+    return new Promise((p_resolve, p_reject) =>
+    {
+        const options = {
+            placeHolder: "Enter Your Gist ID.",
+            password: false,
+            prompt: "Used to sync vscode settings to Gist.",
+            ignoreFocusOut: true
+        };
+        vscode.window.showInputBox(options).then((value) =>
+        {
+            if (value)
+            {
+                p_resolve({ id: value.trim() });
+            }
+            else
+            {
+                p_reject(new Error("Canceled."));
+            }
+        });
+    });
+}
+
 module.exports = {
     status,
     statusInfo,
-    statusError
+    statusError,
+    showGistInputBox,
+    showGitHubTokenInputBox
 };
