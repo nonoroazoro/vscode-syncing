8a4fa08db6198217bcd8523d70b6e2f6e7bf1b49
nonoroazoro <xiaochao.k@gmail.com>
2017-11-29 11:21:08 +0800

    rename to https-proxy-agent

--- @types/alltypes.d.ts
+++
@@ -1,2 +0,0 @@
-// see https://stackoverflow.com/questions/41462729/typescript-react-could-not-find-a-declaration-file-for-module-react-material/41631658#41631658
-declare module "https-proxy-agent";
