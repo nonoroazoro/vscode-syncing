dc1f93773a1dba6a944a2ee4f05fb6ff6a7c4060
nonoroazoro <xiaochao.k@gmail.com>
2018-05-08 16:07:07 +0800

    add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * Constants of `Syncing`.
+ */
+
+// Dot-separated identifiers, same as the secions of VSCode, see `vscode.workspace.getConfiguration`.
+export const CONFIGURATION_KEY = "syncing";
+export const CONFIGURATION_POKA_YOKE_THRESHOLD = "pokaYokeThreshold";
+export const CONFIGURATION_UPLOAD_EXCLUDE = "upload.exclude";
+
+// Dot-separated identifiers, used to access the properties of VSCode settings file.
+export const SETTINGS_POKA_YOKE_THRESHOLD = `${CONFIGURATION_KEY}.${CONFIGURATION_POKA_YOKE_THRESHOLD}`;
+export const SETTINGS_UPLOAD_EXCLUDE = `${CONFIGURATION_KEY}.${CONFIGURATION_UPLOAD_EXCLUDE}`;
