aa737d0c4e57510c03c82618e8d8faf58ec7fa35
nonoroazoro <xiaochao.k@gmail.com>
2018-07-14 16:17:53 +0800

    delete settings-mac.json

--- src/core/Gist.ts
+++ src/core/Gist.ts
@@ -306,6 +306,9 @@ export default class Gist
                     localGist.files = this._getModifiedFiles(localGist.files, remoteGist.files);
                     if (localGist.files)
                     {
+                        // TODO: Should be removed in the next release.
+                        localGist.files["settings-mac.json"] = null;
+
                         // poka-yoke - check if there have been two much changes (more than 10 changes) since the last uploading.
                         const threshold = vscode.workspace.getConfiguration(CONFIGURATION_KEY).get<number>(CONFIGURATION_POKA_YOKE_THRESHOLD);
                         if (threshold > 0)
