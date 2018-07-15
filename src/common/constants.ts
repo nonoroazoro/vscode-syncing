50a0145b1497f213dc74cfe8ca40a423db6a162d
nonoroazoro <xiaochao.k@gmail.com>
2018-07-15 21:52:47 +0800

    upgrade comments

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,8 @@
-// Dot-separated identifiers, same as the secions of VSCode, see `vscode.workspace.getConfiguration`.
+// Dot-separated identifiers, same as the sections of VSCode, see `vscode.workspace.getConfiguration`.
 export const CONFIGURATION_KEY = "syncing";
 export const CONFIGURATION_POKA_YOKE_THRESHOLD = "pokaYokeThreshold";
 export const CONFIGURATION_UPLOAD_EXCLUDE = "upload.exclude";
 
-// Dot-separated identifiers, used to access the properties of VSCode settings file.
+// Dot-separated identifiers, used to access the properties of Syncing's VSCode settings.
 export const SETTINGS_POKA_YOKE_THRESHOLD = `${CONFIGURATION_KEY}.${CONFIGURATION_POKA_YOKE_THRESHOLD}`;
 export const SETTINGS_UPLOAD_EXCLUDE = `${CONFIGURATION_KEY}.${CONFIGURATION_UPLOAD_EXCLUDE}`;
