75fc559f3fd23f6d5bed6f6a97dd8fa100404cc5
nonoroazoro <xiaochao.k@gmail.com>
2018-07-19 12:15:38 +0800

    rename to diffPatch

--- src/core/VSCodeSetting.ts
+++ src/core/VSCodeSetting.ts
@@ -7,7 +7,7 @@ import * as vscode from "vscode";
 import { CONFIGURATION_KEY, CONFIGURATION_POKA_YOKE_THRESHOLD, SETTINGS_UPLOAD_EXCLUDE } from "../common/constants";
 import * as GitHubTypes from "../common/GitHubTypes";
 import { IExtension, ISetting, ISyncedItem, SettingTypes } from "../common/types";
-import { diff } from "../utils/diffHelper";
+import { diff } from "../utils/diffPatch";
 import { excludeSettings, mergeSettings, parse } from "../utils/jsonHelper";
 import Environment from "./Environment";
 import Extension from "./Extension";
