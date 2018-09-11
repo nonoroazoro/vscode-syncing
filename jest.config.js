2cce8e10d41dcf865603fc8b64f44351704f4928
nonoroazoro <xiaochao@gmail.com>
2018-09-11 18:02:24 +0800

    change to jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "coverageDirectory": "<rootDir>/@coverage",
+    "moduleFileExtensions": ["js", "jsx", "ts", "tsx"],
+    "rootDir": ".",
+    "testRegex": "/__tests__/.+\\.(test|spec)\\.ts$",
+    "transform": { "^.+\\.ts$": "ts-jest" },
+    "verbose": true
+};
