4c1161d27e6edad1bdfc5fead649e86c173430b1
nonoroazoro <xiaochao.k@gmail.com>
2018-10-22 15:29:08 +0800

    add webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+const BUILD_PATH = path.join(__dirname, "./dist");
+
+// See https://github.com/Microsoft/vscode/blob/master/extensions/shared.webpack.config.js
+module.exports = {
+    mode: "production",
+    target: "node",
+    node: {
+        __dirname: false
+    },
+    entry: {
+        extension: ["./src/extension"]
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: "[name].js",
+        libraryTarget: "commonjs",
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    externals: {
+        "vscode": "commonjs vscode"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                type: "javascript/auto",
+                test: /\.mjs$/,
+                use: []
+            }
+        ]
+    }
+};
