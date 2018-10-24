07175a3c40ff697137a72a009d46e07ed41fca58
nonoroazoro <xiaochao@gmail.com>
2018-10-24 14:40:38 +0800

    add template util and test

---
+++ src/__tests__/utils/template.test.ts
@@ -0,0 +1,20 @@
+import { format } from "../../utils/template";
+
+describe("Syncing/utils/template", () =>
+{
+    it("replace unique templates with values", () =>
+    {
+        const template = "Hello {0} and {1}!";
+        const values = ["Jack", "Rose"];
+        const target = "Hello Jack and Rose!";
+        expect(format(template, ...values)).toBe(target);
+    });
+
+    it("replace multiple templates with values", () =>
+    {
+        const template = "{0} * {1} is not equal to {0} + {1}";
+        const values = [3, 5];
+        const target = "3 * 5 is not equal to 3 + 5";
+        expect(format(template, ...values)).toBe(target);
+    });
+});
---
+++ src/utils/template.ts
@@ -0,0 +1,22 @@
+/**
+ * Format the template with specified values.
+ *
+ * For example:
+ *
+ * With the template `"Hello, {0} and {1}!"` and the values `["Jack", "Rose"]`, you'll get `"Hello, Jack and Rose!"`.
+ *
+ * @param {string} template The template string.
+ * @param {...any[]} values The values.
+ */
+export function format(template: string, ...values: any[])
+{
+    if (template == null)
+    {
+        return template;
+    }
+
+    return values.reduce((prev, value, index) =>
+    {
+        return prev.replace(new RegExp(`\\{${index}\\}`, "gm"), value);
+    }, template);
+}
