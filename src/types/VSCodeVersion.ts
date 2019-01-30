7d2a060d9aef8bd6c27c0380dcc3543139745475
nonoroazoro <xiaochao@gmail.com>
2019-01-30 15:40:06 +0800

    add VSCodeVersion

---
+++ src/types/VSCodeVersion.ts
@@ -0,0 +1,20 @@
+/**
+ * Represents the versions of VSCode binaries.
+ */
+export enum VSCodeVersion
+{
+    /**
+     * The standard version of VSCode.
+     */
+    Standard,
+
+    /**
+     * The insiders version of VSCode.
+     */
+    Insiders,
+
+    /**
+     * The VSCode under FLOSS license, see [VSCodium](https://github.com/VSCodium/vscodium).
+     */
+    VSCodium
+}
