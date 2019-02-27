8544f1d6cee1b11b9d06ec95c0946dcc6e4e851e
nonoroazoro <xiaochao.k@gmail.com>
2019-02-27 15:26:32 +0800

    add getNormalizedVSCodeLocale

--- src/utils/vscodeAPI.ts
+++ src/utils/vscodeAPI.ts
@@ -1,5 +1,8 @@
 import * as vscode from "vscode";
 
+import { NormalizedLocale } from "../types/NormalizedLocale";
+import { normalize } from "./locale";
+
 /**
  * Gets the VSCode extension by id.
  *
@@ -49,6 +52,14 @@ export function getJSONFormatOnSaveSetting(settingsJSON: any): boolean | undefin
         }
     }
     return result;
+}
+
+/**
+ * Gets the normalized VSCode locale.
+ */
+export function getNormalizedVSCodeLocale(): NormalizedLocale
+{
+    return normalize(getVSCodeLocale());
 }
 
 /**
