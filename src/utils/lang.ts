2cbbb8cb1d5c50550cd9185fd09dd91bdd4bfaa9
nonoroazoro <xiaochao.k@gmail.com>
2019-03-05 16:47:07 +0800

    add lang util and testing

---
+++ src/utils/lang.ts
@@ -0,0 +1,19 @@
+/**
+ * Checks if the input value is an `empty` string.
+ */
+export function isEmptyString(input: any): boolean
+{
+    if (isString(input))
+    {
+        return input.trim() === "";
+    }
+    return false;
+}
+
+/**
+ * Checks if the input value is a `String` primitive or object.
+ */
+export function isString(input: any): input is string
+{
+    return (typeof input === "string" || input instanceof String);
+}
