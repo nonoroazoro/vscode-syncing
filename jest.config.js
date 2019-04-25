9bd7abb24445fb6bbe54bc676e5a4236f49c1939
nonoroazoro <xiaochao.k@gmail.com>
2019-04-25 15:48:56 +0800

    adjust order

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+    "rootDir": ".",
     "coverageDirectory": "<rootDir>/@coverage",
     "moduleFileExtensions": ["js", "ts"],
-    "rootDir": ".",
     "testRegex": "/__tests__/.+\\.(test|spec)\\.ts$",
     "transform": { "^.+\\.ts$": "ts-jest" },
     "verbose": true
