6ff32c94c8f0ca7c6b7e677496be6602cde76849
nonoroazoro <xiaochao.k@gmail.com>
2019-05-05 17:00:44 +0800

    add VSCodeExtensionWatcher

---
+++ src/watcher/VSCodeExtensionWatcher.ts
@@ -0,0 +1,30 @@
+import * as vscode from "vscode";
+
+import { AbstractWatcher, WatcherEvent } from "./AbstractWatcher";
+
+export class VSCodeExtensionWatcher extends AbstractWatcher
+{
+    private _watcher: vscode.Disposable | undefined;
+
+    public async start()
+    {
+        if (!this._watcher)
+        {
+            this._watcher = vscode.extensions.onDidChange(this._handleWatcherEvent);
+        }
+    }
+
+    public async stop()
+    {
+        if (this._watcher)
+        {
+            this._watcher.dispose();
+            this._watcher = undefined;
+        }
+    }
+
+    private _handleWatcherEvent = () =>
+    {
+        this.emit(WatcherEvent.UPDATED);
+    };
+}
