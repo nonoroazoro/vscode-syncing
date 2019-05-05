eb80642523cdc795711f4d8ccfd01535d55a28b4
nonoroazoro <xiaochao.k@gmail.com>
2019-05-05 19:25:15 +0800

    change to string | string[]

--- src/watcher/SettingsWatcher.ts
+++ src/watcher/SettingsWatcher.ts
@@ -4,13 +4,13 @@ import { VSCodeExtensionWatcher } from "./VSCodeExtensionWatcher";
 
 export class SettingsWatcher extends AbstractWatcher<WatcherEvent.ALL>
 {
-    private _paths: string[];
+    private _paths: string | string[];
     private _excludes: string[] | undefined;
 
     private _fileWatcher: ChokidarFileWatcher | undefined;
     private _extensionWatcher: VSCodeExtensionWatcher | undefined;
 
-    constructor(paths: string[], excludes?: string[])
+    constructor(paths: string | string[], excludes?: string[])
     {
         super();
         this._paths = paths;
