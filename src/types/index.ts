d97500a429d87a57d5d9aa11ede5c534c0d7eb24
Nonoroazoro <xiaochao.k@gmail.com>
2022-05-11 15:32:47 +0800

    export types

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./GitHubTypes";
+export * from "./NormalizedLocale";
+export * from "./Platform";
+export * from "./SyncingTypes";
+export * from "./VSCodeEdition";
+export * from "./VSCodeWebAPITypes";
